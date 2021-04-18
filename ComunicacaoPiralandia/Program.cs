3d2536b2335b7619e7b5cd5e17681956d72bff2c
KLauss Rodrigo Diorio <krdiorio@outlook.com>
2021-04-17 21:02:27 -0300

    Codigo final

---
+++ ComunicacaoPiralandia/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ComunicacaoPiralandia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+
+            char[] arr = n.ToCharArray();
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                System.Console.Write(arr[i]);
+            }
+
+        }
+    }
+}
